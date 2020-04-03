6446e9fca1e8bd6d9078e07a045c98d82a18c338
Andrzej Popek <andrzejpopek93@gmail.com>
2020-04-03 16:14:32 +0200

    add posts sketch

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -1,42 +1,68 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
 
 import clsx from 'clsx';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { connect } from 'react-redux';
+import { getAll } from '../../../redux/postsRedux.js';
 
 import styles from './Homepage.module.scss';
 
-const Component = ({ className, children }) => (
+const Component = ({ className, posts }) => (
   <div className={clsx(className, styles.root)}>
     {/* for logged in*/}
     <Button variant="contained" color="primary" href="/post/add">
       Add new
     </Button>
     {/* for all*/}
-    <div>List of adds.....</div>
+    <div>
+      {posts.map(post => (
+        <Card key={post.id} variant="outlined">
+          <CardActionArea>
+            <CardMedia component="img" alt="Contemplative Reptile" height="140" image={post.image} title="Contemplative Reptile" />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {post.title}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {post.text}
+              </Typography>
+              <Typography variant="body1" color="textPrimary" component="p">
+                {`price: $${post.price}`}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary" href={`/post/${post.id}`}>
+              Details
+            </Button>
+          </CardActions>
+        </Card>
+      ))}
+    </div>
   </div>
 );
 
 Component.propTypes = {
-  children: PropTypes.node,
   className: PropTypes.string,
+  posts: PropTypes.array,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  posts: getAll(state),
+});
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
-
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps)(Component);
 
 export {
-  Component as Homepage,
-  // Container as Homepage,
+  // Component as Homepage,
+  Container as Homepage,
   Component as HomepageComponent,
 };
--- src/components/views/Post/Post.js
+++ src/components/views/Post/Post.js
@@ -1,37 +1,77 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
 
 import clsx from 'clsx';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { connect } from 'react-redux';
+import { getAll } from '../../../redux/postsRedux.js';
 
 import styles from './Post.module.scss';
 
-const Component = ({className, children}) => (
+const Component = ({ className, posts, match }) => (
   <div className={clsx(className, styles.root)}>
-    <h2>Post</h2>
-    {children}
+    {/* for logged in*/}
+    <Button variant="contained" color="primary" href="/post/add">
+      Add new
+    </Button>
+    {/* for all*/}
+    <div>
+      <Card key={posts[match.params.id - 1].id} variant="outlined">
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="Contemplative Reptile"
+            height="140"
+            image={posts[match.params.id - 1].image}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {posts[match.params.id - 1].title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {posts[match.params.id - 1].text}
+            </Typography>
+            <Typography variant="body1" color="textPrimary" component="p">
+              {`price: $${posts[match.params.id - 1].price}`}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary" href={`/post/${posts[match.params.id - 1].id}`}>
+            Details
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
   </div>
 );
 
 Component.propTypes = {
-  children: PropTypes.node,
   className: PropTypes.string,
+  posts: PropTypes.array,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }),
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  posts: getAll(state),
+});
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
-
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps)(Component);
 
 export {
-  Component as Post,
-  // Container as Post,
+  // Component as Post,
+  Container as Post,
   Component as PostComponent,
 };
