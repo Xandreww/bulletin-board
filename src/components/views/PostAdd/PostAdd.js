6cca3afcc2db41c1116324b8d3f15d504e5ab948
Andrzej Popek <andrzejpopek93@gmail.com>
2020-04-06 16:11:47 +0200

    add PostAdd with Redux (not working)

--- src/components/views/PostAdd/PostAdd.js
+++ src/components/views/PostAdd/PostAdd.js
@@ -7,50 +7,123 @@ import PhotoCamera from '@material-ui/icons/PhotoCamera';
 
 import clsx from 'clsx';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { connect } from 'react-redux';
+import { addPost } from '../../../redux/postsRedux.js';
 
 import styles from './PostAdd.module.scss';
 
-const Component = ({ className }) => (
-  <div className={clsx(className, styles.root)}>
-    <form autoComplete="off">
-      <h2 className={styles.title}>Add new post</h2>
-      <TextField className={styles.formField} required id="title" label="title" />
-      <TextField className={styles.formField} required id="price" label="price" type="number" />
-      <TextField className={styles.content} required id="content" label="content" multiline rows="10" variant="outlined" />
-      <TextField className={styles.formField} required id="email" type="email" label="email" />
-      <TextField className={styles.formField} required id="telephone" type="number" label="telephone number" />
-      <input accept="image/*" id="icon-button-file" type="file" />
-      <label htmlFor="icon-button-file">
-        <IconButton className={styles.addPhoto} color="primary" aria-label="upload picture" component="span">
-          <PhotoCamera />
-        </IconButton>
-      </label>
-      <Button variant="contained" color="primary">
-        Submit
-      </Button>
-    </form>
-  </div>
-);
+class Component extends React.Component {
+  state = {
+    title: '',
+    price: '',
+    content: '',
+    email: '',
+    telephone: '',
+    image: undefined,
+  };
 
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-};
+  static propTypes = {
+    className: PropTypes.string,
+    addPost: PropTypes.func,
+  };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+  handleChange = ({ target }) => {
+    console.log('target type:', target.type);
+    console.log('target value:', target.value);
+    console.log(this.state);
+    switch (target.type) {
+      case 'text': {
+        this.setState({ title: target.value });
+        break;
+      }
+      case 'number': {
+        this.setState({ price: target.value });
+        break;
+      }
+      case 'textarea': {
+        this.setState({ content: target.value });
+        break;
+      }
+      case 'email': {
+        this.setState({ email: target.value });
+        break;
+      }
+      case 'tel': {
+        this.setState({ telephone: target.value });
+        break;
+      }
+      case 'file': {
+        this.setState({ telephone: target.value });
+        break;
+      }
+      default:
+        return console.log('no switch type');
+    }
+  };
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Submit!', this.state);
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+    this.props.addPost(this.state);
+  };
+
+  render() {
+    const { handleChange, handleSubmit } = this;
+    const { className } = this.props;
+    const { title, price, content, email, telephone, image } = this.state;
+
+    return (
+      <div className={clsx(className, styles.root)}>
+        <form autoComplete="off" onSubmit={(event) => handleSubmit(event)}>
+          <h2 className={styles.title}>Add new post</h2>
+          <TextField className={styles.formField} required id="title" label="title" minLength="10" value={title} onChange={handleChange} />
+          <TextField className={styles.formField} required id="price" label="price" type="number" value={price} onChange={handleChange} />
+          <TextField
+            className={styles.content}
+            required
+            id="content"
+            label="content"
+            multiline
+            rows="10"
+            variant="outlined"
+            minLength="20"
+            value={content}
+            onChange={handleChange}
+          />
+          <TextField className={styles.formField} required id="email" type="email" label="email" value={email} onChange={handleChange} />
+          <TextField
+            className={styles.formField}
+            required
+            id="telephone"
+            type="tel"
+            label="telephone number"
+            value={telephone}
+            onChange={handleChange}
+          />
+          <input accept="image/*" id="icon-button-file" type="file" value={image} onChange={handleChange} />
+          <label htmlFor="icon-button-file">
+            <IconButton className={styles.addPhoto} color="primary" aria-label="upload picture" component="span">
+              <PhotoCamera />
+            </IconButton>
+          </label>
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  addPost: (payload) => dispatch(addPost(payload)),
+});
+
+const Container = connect(mapDispatchToProps)(Component);
 
 export {
-  Component as PostAdd,
-  // Container as PostAdd,
+  // Component as PostAdd,
+  Container as PostAdd,
   Component as PostAddComponent,
 };
